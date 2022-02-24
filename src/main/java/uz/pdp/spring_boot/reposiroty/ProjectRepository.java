838ad3217975f114448dc6fb2dc6283d0606d217
Shahboz <codershaha@gmail.com>
2022-02-24 13:20:52 +0500

    task finsh

--- src/main/java/uz/pdp/spring_boot/reposiroty/ProjectRepository.java
+++ src/main/java/uz/pdp/spring_boot/reposiroty/ProjectRepository.java
@@ -3,12 +3,6 @@ package uz.pdp.spring_boot.reposiroty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import uz.pdp.spring_boot.entity.project.Project;
-
-
-public interface ProjectRepository extends JpaRepository<Project, Long>, AbstractRepository{
-
-@Query(value = "from Project  p where p.id=:projectId")
-    Project findProjectById(Long projectId);
 import org.springframework.data.repository.query.Param;
 import uz.pdp.spring_boot.entity.column.Colum;
 import uz.pdp.spring_boot.entity.organization.Organization;
@@ -16,7 +10,10 @@ import uz.pdp.spring_boot.entity.project.Project;
 
 import java.util.List;
 
+
 public interface ProjectRepository extends JpaRepository<Project, Long>, AbstractRepository {
+    @Query(value = "from Project  p where p.id=:projectId")
+    Project findProjectById(Long projectId);
 
     @Query(value = "from Organization o where o.id=:id")
     Organization getOrg(@Param("id") Long id);
